9f2cc8a9b99903ca75af5b66e35bdaff6df811d0
Vincent Liu <vincent.liu.tw@gmail.com>
2026-06-15 15:19:49 +0800

    update merge_pdf.py to exclude residents in no_paper.txt file

--- merge_pdf.py
+++ merge_pdf.py
@@ -3,9 +3,20 @@ import glob
 
 # List all .pdf files in the pdf directory
 pdf_files = glob.glob(".\pdf\*.pdf")
+
+# read list from no_paper.txt
+with open("no_paper.txt", "r") as file:
+    no_paper_list = [line.strip() for line in file if line.strip()]
+
+filtered_list = [
+    item for item in pdf_files 
+    if not any(sub in item for sub in no_paper_list)
+]
+
+# define merger as pdf writer
 merger = PdfWriter()
 
-for pdf in pdf_files:
+for pdf in filtered_list:
     merger.append(pdf)
 
 # Write the final result to a new file
